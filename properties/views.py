8effde71b083efc41dd54b69d7cea501e61b56db
VictorRop4 <victorrop99@gmail.com>
2025-09-28 15:39:48 +0300

    Edited view.py file

--- properties/views.py
+++ properties/views.py
@@ -12,10 +12,18 @@ def property_list(request):
     return render(request, "properties/property_list.html", {"properties": properties})
 
 
+
 def property_list(request):
     properties = get_all_properties()
-    return render(request, "properties/property_list.html", {"properties": properties})
-
-def cache_metrics(request):
-    metrics = get_redis_cache_metrics()
-    return JsonResponse(metrics)
+    data = [
+        {
+            "id": prop.id,
+            "title": prop.title,
+            "description": prop.description,
+            "price": str(prop.price),
+            "location": prop.location,
+            "created_at": prop.created_at.isoformat(),
+        }
+        for prop in properties
+    ]
+    return JsonResponse({",": data})
